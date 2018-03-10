7757bb5c9116672ef49ec48ca9397f90dbe53502
Sarah McGrath <sarah@shmcgrath.com>
2018-03-09 23:34:27 -0500

    Remove Imperial March call from Raise keypress

--- keyboards/planck/keymaps/shmcg/keymap.c
+++ keyboards/planck/keymaps/shmcg/keymap.c
@@ -137,9 +137,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case RAISE:
       if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(imperial_march_song);
-        #endif
         layer_on(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
